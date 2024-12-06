be0d217bc102644e5268176f2bd0626b044a59d6
glassBead <ben.nims@thesoapstone.net>
2024-12-06 02:52:23 -0600

    Remove remaining junk code and empty folders.

--- atris-backend-audius/src/hooks/useSdk.ts
+++
@@ -1,9 +0,0 @@
-import { sdk } from '@audius/sdk'
-
-const apiKey = import.meta.env.AUDIUS_API_KEY as string
-
-const instance = sdk({
-  apiKey
-})
-
-export const useSdk = () => ({ sdk: instance })
